d232d43a572b08201a304cb432f4bc055f8ecac2
Rakka Rage <rakkarage@gmail.com>
2020-05-05 19:45:19 -0400

    x

--- PixelInterface/Connect/Status.gd
+++ PixelInterface/Connect/Status.gd
@@ -1,6 +1,8 @@
 extends Panel
 
 const _time = 0.333
+const _trans = Tween.TRANS_EXPO
+const _ease = Tween.EASE_IN
 
 func _ready():
 	Utility.ok(connect("mouse_entered", self, "mouseEntered"))
@@ -8,20 +10,19 @@ func _ready():
 	Utility.ok($Panel.connect("mouse_entered", self, "mouseEntered"))
 	Utility.ok($Panel.connect("mouse_exited", self, "mouseExited"))
 
-func setEmail(email: String):
-	$Panel/Email.text = email
+func setEmail(email: String): $Panel/Email.text = email
 
 func mouseEntered():
 	if Firebase.authenticated():
 		$Tween.stop($Panel, "rect_rotation");
-		$Tween.interpolate_property($Panel, "rect_rotation", null, 0, _time, Tween.TRANS_EXPO, Tween.EASE_IN)
+		$Tween.interpolate_property($Panel, "rect_rotation", null, 0, _time, _trans, _ease)
 		$Tween.stop($Panel, "modulate");
-		$Tween.interpolate_property($Panel, "modulate", null, Color.white, _time, Tween.TRANS_EXPO, Tween.EASE_IN)
+		$Tween.interpolate_property($Panel, "modulate", null, Color.white, _time, _trans, _ease)
 		$Tween.start()
 
 func mouseExited():
 	$Tween.stop($Panel, "rect_rotation");
-	$Tween.interpolate_property($Panel, "rect_rotation", null, -90, _time, Tween.TRANS_EXPO, Tween.EASE_IN)
+	$Tween.interpolate_property($Panel, "rect_rotation", null, -90, _time, _trans, _ease)
 	$Tween.stop($Panel, "modulate");
-	$Tween.interpolate_property($Panel, "modulate", null, Color(0, 0, 0, 0), _time, Tween.TRANS_EXPO, Tween.EASE_IN)
+	$Tween.interpolate_property($Panel, "modulate", null, Color(0, 0, 0, 0), _time, _trans, _ease)
 	$Tween.start()

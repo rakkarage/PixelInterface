8673d5f229118c8a127154af27f17c3bb4354ebd
Rakka Rage <rakkarage@gmail.com>
2020-05-08 21:29:49 -0400

    x

--- PixelInterface/Cursor.gd
+++ PixelInterface/Cursor.gd
@@ -1,11 +1,51 @@
 extends Node2D
 
-onready var _sprite = $Sprite
+onready var _sprite := $Sprite
 
-func _ready():
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_default_cursor_shape(Input.CURSOR_IBEAM)
 
-func _process(delta):
-	var p = get_global_mouse_position()
-	if Input.is_action_pressed("mb_left"):
-		_sprite.play("Click")
+func _process(_delta) -> void:
+	global_position = get_global_mouse_position()
+	match Input.get_current_cursor_shape():
+		Input.CURSOR_ARROW:
+			if Input.is_action_just_pressed("mb_left"):
+				_sprite.play("ArrowClick")
+			else:
+				_sprite.play("Arrow")
+		Input.CURSOR_IBEAM:
+			_sprite.play("Caret")
+		Input.CURSOR_POINTING_HAND:
+			if Input.is_action_just_pressed("mb_left"):
+				_sprite.play("PointClick")
+			else:
+				_sprite.play("Point")
+		Input.CURSOR_CROSS:
+			_sprite.play("Cross")
+		Input.CURSOR_WAIT:
+			_sprite.play("Wait")
+		Input.CURSOR_BUSY:
+			_sprite.play("Busy")
+		Input.CURSOR_DRAG:
+			_sprite.play("Drag")
+		Input.CURSOR_CAN_DROP:
+			_sprite.play("Arrow")
+		Input.CURSOR_FORBIDDEN:
+			_sprite.play("Drop")
+		Input.CURSOR_VSIZE:
+			_sprite.play("SizeV")
+		Input.CURSOR_HSIZE:
+			_sprite.play("SizeH")
+		Input.CURSOR_BDIAGSIZE:
+			_sprite.play("SizeDiagBack")
+		Input.CURSOR_FDIAGSIZE:
+			_sprite.play("SizeDiagFore")
+		Input.CURSOR_MOVE:
+			_sprite.play("Move")
+		Input.CURSOR_VSPLIT:
+			_sprite.play("SplitV")
+		Input.CURSOR_HSPLIT:
+			_sprite.play("SplitH")
+		Input.CURSOR_HELP:
+			_sprite.play("Help")

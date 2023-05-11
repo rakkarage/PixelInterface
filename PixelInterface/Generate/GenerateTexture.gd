bf0d95f9b43f3d1b09364e568deb250f2120ad74
Rakka Rage <rakkarage@gmail.com>
2023-05-11 19:07:58 -0400

    x

--- PixelInterface/Generate/GenerateTexture.gd
+++ PixelInterface/Generate/GenerateTexture.gd
@@ -13,10 +13,10 @@ extends Control
 @onready var _seedSlider: HSlider = $Margin/HBox/Left/Settings/Seed/HSlider
 @onready var _octavesEdit: SpinBox = $Margin/HBox/Left/Settings/Octaves/SpinBox
 @onready var _octavesSlider: HSlider = $Margin/HBox/Left/Settings/Octaves/HSlider
-@onready var _periodEdit: SpinBox = $Margin/HBox/Left/Settings/Period/SpinBox
-@onready var _periodSlider: HSlider = $Margin/HBox/Left/Settings/Period/HSlider
-@onready var _persistenceEdit: SpinBox = $Margin/HBox/Left/Settings/Persistence/SpinBox
-@onready var _persistenceSlider: HSlider = $Margin/HBox/Left/Settings/Persistence/HSlider
+@onready var _frequencyEdit: SpinBox = $Margin/HBox/Left/Settings/Frequency/SpinBox
+@onready var _frequencySlider: HSlider = $Margin/HBox/Left/Settings/Frequency/HSlider
+@onready var _gainEdit: SpinBox = $Margin/HBox/Left/Settings/Gain/SpinBox
+@onready var _gainSlider: HSlider = $Margin/HBox/Left/Settings/Gain/HSlider
 @onready var _lacunarityEdit: SpinBox = $Margin/HBox/Left/Settings/Lacunarity/SpinBox
 @onready var _lacunaritySlider: HSlider = $Margin/HBox/Left/Settings/Lacunarity/HSlider
 
@@ -25,7 +25,6 @@ extends Control
 @onready var _saveButton: Button = $Margin/HBox/Left/Buttons/Save
 
 @export var _size := 128
-@export var _seedMax := 65535
 @export var _path := "res://PixelInterface/GenerateTexture.png"
 @export var _noise : FastNoiseLite
 
@@ -40,12 +39,12 @@ func _ready() -> void:
 	_octavesSlider.share(_octavesEdit)
 	_octavesEdit.connect("value_changed", Callable(self, "_octavesEditChanged"))
 	_octavesSlider.connect("value_changed", Callable(self, "_octavesSliderChanged"))
-	_periodSlider.share(_periodEdit)
-	_periodEdit.connect("value_changed", Callable(self, "_periodEditChanged"))
-	_periodSlider.connect("value_changed", Callable(self, "_periodSliderChanged"))
-	_persistenceSlider.share(_persistenceEdit)
-	_persistenceEdit.connect("value_changed", Callable(self, "_persistenceEditChanged"))
-	_persistenceSlider.connect("value_changed", Callable(self, "_persistenceSliderChanged"))
+	_frequencySlider.share(_frequencyEdit)
+	_frequencyEdit.connect("value_changed", Callable(self, "_frequencyEditChanged"))
+	_frequencySlider.connect("value_changed", Callable(self, "_frequencySliderChanged"))
+	_gainSlider.share(_gainEdit)
+	_gainEdit.connect("value_changed", Callable(self, "_gainEditChanged"))
+	_gainSlider.connect("value_changed", Callable(self, "_gainSliderChanged"))
 	_lacunaritySlider.share(_lacunarityEdit)
 	_lacunarityEdit.connect("value_changed", Callable(self, "_lacunarityEditChanged"))
 	_lacunaritySlider.connect("value_changed", Callable(self, "_lacunaritySliderChanged"))
@@ -53,19 +52,19 @@ func _ready() -> void:
 	_resetButton.connect("pressed", Callable(self, "_resetPressed"))
 	_saveButton.connect("pressed", Callable(self, "_savePressed"))
 	_noise = FastNoiseLite.new()
+	_noise.seed = randi()
 	_preview.get_material().set_shader_parameter("color", Color.WHITE)
 	_big.get_material().set_shader_parameter("color", Color.WHITE)
 	_output = Image.create(_size, _size, false, Image.FORMAT_RGBA8)
 	call_deferred("_loadSettings")
 
 func _generatePressed() -> void:
-	# TODO: remove seed from options if not even really using it?
 	var old := _noise.seed
-	_noise.seed += randi() % _seedMax
+	# _noise.seed += 0
 	var image1 := _noise.get_seamless_image(_size, _size)
-	_noise.seed += randi() % _seedMax
+	_noise.seed += 1
 	var image2 := _noise.get_seamless_image(_size, _size)
-	_noise.seed += randi() % _seedMax
+	_noise.seed += 2
 	var image3 := _noise.get_seamless_image(_size, _size)
 	_noise.seed = old;
 	for y in range(_size):
@@ -99,13 +98,12 @@ func _loadSettings() -> void:
 	_seedSlider.value = _noise.seed
 	_octavesEdit.value = _noise.fractal_octaves
 	_octavesSlider.value = _noise.fractal_octaves
-	# _periodEdit.value = _noise.period
-	# _periodSlider.value = _noise.period
-	# _persistenceEdit.value = _noise.persistence
-	# _persistenceSlider.value = _noise.persistence
-	# _lacunarityEdit.value = _noise.lacunarity
-	# _lacunaritySlider.value = _noise.lacunarity
-	# TODO: !?
+	_frequencyEdit.value = _noise.frequency
+	_frequencySlider.value = _noise.frequency
+	_gainEdit.value = _noise.fractal_gain
+	_gainSlider.value = _noise.fractal_gain
+	_lacunarityEdit.value = _noise.fractal_lacunarity
+	_lacunaritySlider.value = _noise.fractal_lacunarity
 	_generatePressed()
 
 func _colorEditChanged(value: String) -> void:
@@ -139,32 +137,32 @@ func _octavesSliderChanged(value: float) -> void:
 	_octavesEdit.value = value
 	call_deferred("_generatePressed")
 
-func _periodEditChanged(value: float) -> void:
-	_noise.period = value
-	_periodSlider.value = value
+func _frequencyEditChanged(value: float) -> void:
+	_noise.frequency = value
+	_frequencySlider.value = value
 	call_deferred("_generatePressed")
 
-func _periodSliderChanged(value: float) -> void:
-	_noise.period = value
-	_periodEdit.value = value
+func _frequencySliderChanged(value: float) -> void:
+	_noise.frequency = value
+	_frequencyEdit.value = value
 	call_deferred("_generatePressed")
 
-func _persistenceEditChanged(value: float) -> void:
-	_noise.persistence = value
-	_persistenceSlider.value = value
+func _gainEditChanged(value: float) -> void:
+	_noise.fractal_gain = value
+	_gainSlider.value = value
 	call_deferred("_generatePressed")
 
-func _persistenceSliderChanged(value: float) -> void:
-	_noise.persistence = value
-	_persistenceEdit.value = value
+func _gainSliderChanged(value: float) -> void:
+	_noise.fractal_gain = value
+	_gainEdit.value = value
 	call_deferred("_generatePressed")
 
 func _lacunarityEditChanged(value: float) -> void:
-	_noise.lacunarity = value
+	_noise.fractal_lacunarity = value
 	_lacunaritySlider.value = value
 	call_deferred("_generatePressed")
 
 func _lacunaritySliderChanged(value: float) -> void:
-	_noise.lacunarity = value
+	_noise.fractal_lacunarity = value
 	_lacunarityEdit.value = value
 	call_deferred("_generatePressed")
